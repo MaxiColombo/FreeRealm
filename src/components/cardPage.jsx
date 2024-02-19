fbbc8bdad74528911ac02b97239ffc9615aad830
MaxiColombo <maximocolombo2003@gmail.com>
2024-02-19 19:03:54 -0300

    Delete vercel routes, css changes in card

--- src/components/cardPage.jsx
+++ src/components/cardPage.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import "./cardPage.css"
+import categories from "../assets/category.json"
 
 export function CardPage({ items }) {
-  const [loading,setLoading]= useState(true)
+  const location = useLocation()
+  const [loading, setLoading] = useState(true)
+
+  const getCategoryColor = () => {
+    const currentCategory = categories.find(category => category.nameID === location.pathname.slice(1));
+    return currentCategory ? currentCategory.color : '#000';
+  }
+
   return (
     <div className="containerCard">
       {items.map((item, index) => (
-        <div className="item" key={index}>
+        <div className="item" key={index} style={{ '--hover-color': getCategoryColor() }}>
           {loading && <div className="loading-icon">Cargando...</div>}
           <img
             src={item.image}
